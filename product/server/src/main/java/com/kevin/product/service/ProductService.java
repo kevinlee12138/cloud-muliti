35b3690990ff7aa9430669fb5d6f730f2518f3e2
lilei <kevin12138az@163.com>
2018-12-29 23:07:29 +0800

    server init product-multi init

---
+++ product/server/src/main/java/com/kevin/product/service/ProductService.java
@@ -0,0 +1,24 @@
+package com.kevin.product.service;
+
+
+import com.kevin.product.common.ProductInfoOutput;
+import com.kevin.product.dataObject.ProductInfo;
+import com.kevin.product.dto.DecreaseStockInput;
+
+import java.util.List;
+
+/**
+ * Created by 廖师兄
+ * 2017-12-09 21:57
+ */
+public interface ProductService {
+
+    /**
+     * 查询所有在架商品列表
+     */
+    List<ProductInfo> findUpAll();
+
+    List<ProductInfoOutput> findList(List<String> productIdList);
+
+    void decreaseStock(List<DecreaseStockInput> cartDtoList);
+}
---
+++ product/server/src/main/java/com/kevin/product/service/impl/ProductServiceImpl.java
@@ -0,0 +1,71 @@
+package com.kevin.product.service.impl;
+
+
+import com.kevin.product.common.ProductInfoOutput;
+import com.kevin.product.dataObject.ProductInfo;
+import com.kevin.product.dto.DecreaseStockInput;
+import com.kevin.product.enums.ProductStatusEnum;
+import com.kevin.product.enums.ResultEnum;
+import com.kevin.product.exception.ProductException;
+import com.kevin.product.repository.ProductInfoRepository;
+import com.kevin.product.service.ProductService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * Created by 廖师兄
+ * 2017-12-09 21:59
+ */
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductInfoRepository productInfoRepository;
+
+
+    @Override
+    public List<ProductInfo> findUpAll() {
+        return productInfoRepository.findByProductStatus(ProductStatusEnum.UP.getCode());
+    }
+
+    @Override
+    public List<ProductInfoOutput> findList(List<String> productIdList) {
+        return productInfoRepository.findByProductIdIn(productIdList).stream()
+                .map(e ->{
+                    ProductInfoOutput output = new ProductInfoOutput();
+                    BeanUtils.copyProperties(e,output);
+                    return output;
+                })
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    @Transactional
+    public void decreaseStock(List<DecreaseStockInput> decreaseStockInputs) {
+        for (DecreaseStockInput decreaseStockInput:decreaseStockInputs){
+            Optional<ProductInfo> productInfoOptional =  productInfoRepository.findById(decreaseStockInput.getProductId());
+            //商品不存在
+            if (!productInfoOptional.isPresent()){
+                throw new ProductException(ResultEnum.PRODUCT_NOT_EXIST);
+            }
+
+            ProductInfo productInfo = productInfoOptional.get();
+            //判断库存够不够
+            Integer result = productInfo.getProductStock() - decreaseStockInput.getProductQuantity();
+            if (result < 0){
+                throw new ProductException(ResultEnum.PRODUCT_STOCK_ERROR);
+            }
+
+            productInfo.setProductStock(result);
+            productInfoRepository.save(productInfo);
+        }
+    }
+
+
+}
